35181179d846687bd69562dda2df7af20c6d27b1
Andrew & Candace <akayvanfar@gmail.com & candace.schenk@gmail.com>
2014-03-31 09:09:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialThingamajig::Application.config.secret_key_base = '80a12ec3722e922276513bffa1f9c92ae11bbd2e02135f3d05930b18e46470e0da1f085629ad3f6f3905fd30ba1e1380ead13af2274a71fa1262ab807bd0a1c7'
